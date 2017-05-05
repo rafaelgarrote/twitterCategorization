e523dfdfdc03ffb66c625aec2f3623e844b587ae
rafaelgarrote <rgarrotehe@gmail.com>
2017-05-05 20:48:41 +0200

    fin de dia

--- src/main/java/com/utad/twittercategorization/Main.java
+++ src/main/java/com/utad/twittercategorization/Main.java
@@ -1,7 +1,14 @@
 package com.utad.twittercategorization;
 
+import com.utad.twittercategorization.utils.GeoUtils;
+
 /**
  * Created by utad on 5/05/17.
  */
 public class Main {
+
+    public static void main(String [ ] args) {
+        String response = GeoUtils.getCity("40.714224","-73.961452");
+        System.out.println(response);
+    }
 }
---
+++ src/main/java/com/utad/twittercategorization/utils/GeoUtils.java
@@ -0,0 +1,34 @@
+package com.utad.twittercategorization.utils;
+
+import org.json.JSONObject;
+
+/**
+ * Created by utad on 5/05/17.
+ */
+public class GeoUtils {
+
+    private static String GOOGLE_URL = "http://maps.googleapis.com/maps/api/geocode/json?latlng=";
+    private static String SENSOR = "&sensor=false";
+
+    public static String getCity(String latitude, String longitude) {
+
+        String result = "";
+        try {
+            String url = GOOGLE_URL + latitude + "," + longitude + SENSOR;
+            result = HTTPUtils.getHTML(url);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        parseResult(result);
+        return result;
+
+    }
+
+    private static String parseResult(String result) {
+        JSONObject obj = new JSONObject(result);
+        JSONObject ll = (JSONObject) obj.getJSONArray("results").get(0);
+        ll.getJSONArray("address_components")
+        return "";
+    }
+
+}
