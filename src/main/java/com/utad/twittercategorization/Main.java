6f56f4eec746b38dc1aec4a2f00512869f324f0b
marcosdm <marcos.dmartin@gmail.com>
2017-05-06 11:02:32 +0200

    Método para extraer entidades del tweet

--- src/main/java/com/utad/twittercategorization/Main.java
+++ src/main/java/com/utad/twittercategorization/Main.java
@@ -1,7 +1,26 @@
 package com.utad.twittercategorization;
 
+import com.utad.twittercategorization.utils.*;
+
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
 /**
  * Created by utad on 5/05/17.
  */
 public class Main {
+    public static void main(String [ ] args) throws Exception {
+
+        HashMap myEntities = EntitiesUtils.hagoGet("Bonito viaje en París, ha sido mejor que el de Londres. Me ha gustado el Louvre y los cuadros de Da Vinci");
+
+        for (Object name: myEntities.keySet()){
+            String key =name.toString();
+            ArrayList value = (ArrayList) myEntities.get(name);
+            System.out.println(key + " " + value);
+        }
+    }
+
 }
